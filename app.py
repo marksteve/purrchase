ae04cad0cd8b963843c56747ac87111fa78f8ee9
Kat Padilla <katpadillaaa@gmail.com>
2014-06-03 17:29:15 +0800

    di ko gets flow

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import os
-import constants
+import const
 
 from flask import Flask, request, jsonify, render_template
 #from redis import StrictRedis
@@ -15,21 +15,34 @@ def index():
 # ==== /oauth ==== #
 @app.route('/oauth', methods=['GET'])
 def oauth_uri():
-  test = get_access_token()
-  return test
+  access_token = get_access_token()
+  return access_token
+
+def get_code():
+  # nu to?
+  return true
 
 def get_access_token():
+  app_id = 'oyap4tKpyBqf4RcLkgiyzefLnae6tMj7'
+  app_secret ='256e555ba0d3d76c933bc56fae30c2192e2880557625ab8fe947429e7383a3e5'
+  code = get_code()
+
+  params = {'app_id': app_id, 'app_secret': app_secret, 'code': code}
+  r = requests.post(const.G_AUTH_ENDPOINT, data=params)
   return "ACCESS_TOKEN"
+# ==== END /oauth ==== #
 
 # ==== /sms ==== #
 @app.route('/sms', methods=['POST'])
 def sms_uri():
   return "SMS"
+# ==== END /sms ==== #
 
 # ==== /charge ==== #
 @app.route('/charge', methods=['POST'])
 def charge_uri():
   return "CHARGE"
+# ==== END /charge ==== #
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", debug=True)
---
+++ const.py
@@ -0,0 +1,7 @@
+G_ENDPOINT = 'http://devapi.globelabs.com.ph/'
+G_VERSION  = 'v1'
+SMS_MSG = "test msg"
+
+# end fuckin points
+G_CHARGING_ENDPOINT = G_ENDPOINT + 'payment/' + G_VERSION + '/transactions/amount'
+G_AUTH_ENDPOINT = 'http://developer.globelabs.com.ph/oauth/access_token?'
