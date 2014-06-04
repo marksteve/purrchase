fe6b01a7952861725975225ac80bb8fc8cb45e4e
Mark Steve Samson <hello@marksteve.com>
2014-06-05 00:38:05 +0800

    Move config to a file and add simpleflake

--- app.py
+++ app.py
@@ -1,26 +1,27 @@
 import os
-import const
-import uuid
 import time
 
-from flask import Flask, request, json, jsonify, render_template
-from redis import StrictRedis
+import const
 import requests
+from flask import abort, Flask, json, jsonify, render_template, request
+from redis import StrictRedis
+from simpleflake import simpleflake
 
 app = Flask(__name__)
-r = StrictRedis(host=os.environ.get('REDIS_PORT_6379_TCP_ADDR'))
+app.config.from_pyfile('config.py')
+db = StrictRedis(host=os.environ.get('REDIS_PORT_6379_TCP_ADDR'))
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/opm', methods=['GET', 'POST'])
+@app.route('/opm')
 def opm():
   return render_template('opm.html')
 
 # ==== /oauth ==== #
-@app.route('/oauth', methods=['GET'])
-def oauth_uri():
+@app.route('/oauth')
+def oauth():
   access_token = get_access_token()
   return access_token
 
@@ -28,81 +29,88 @@ def oauth_uri():
 # returns json data of subscriber_number & access_token
 def get_access_token():
   # Hackathon "merchant" sample details
-  app_id     = 'oyap4tKpyBqf4RcLkgiyzefLnae6tMj7'
-  app_secret = '256e555ba0d3d76c933bc56fae30c2192e2880557625ab8fe947429e7383a3e5'
-
-  code       = request.args['code']
-
-  params = {'app_id': app_id, 'app_secret': app_secret, 'code': code}
-  res    = requests.post(const.G_AUTH_ENDPOINT, data=params)
-
-  x = res.json()
-  #r.delete(x['subscriber_number'])
-  r.hset(x['subscriber_number'], 'access_token', x['access_token'])
-
-  return jsonify(**x)
+  code = request.args['code']
+  data = {
+    'app_id': app.config['GLOBE_APP_ID'],
+    'app_secret': app.config['GLOBE_APP_SECRET'],
+    'code': code,
+  }
+  res = requests.post(const.G_AUTH_ENDPOINT, data=data)
+  if res.ok:
+    payload = res.json()
+    db.hset(
+      payload['subscriber_number'],
+      'access_token',
+      payload['access_token'],
+    )
+    return jsonify(**payload)
+  else:
+    abort(500)
 # ==== END /oauth ==== #
 
 # ==== /sms ==== #
 @app.route('/sms', methods=['POST', 'GET'])
-def sms_uri():
+def sms():
   return "boo!"
 # ==== END /sms ==== #
 
 # ==== /charge ==== #
-"""
-Charge URI is the endpoint for charging and sending of sms
-This endpoint expects the ff params:
- - msisdn
- - amount
-"""
-@app.route('/charge', methods=['POST','GET'])
-def charge_uri():
+@app.route('/charge', methods=['POST'])
+def charge():
+  """
+  Charge URI is the endpoint for charging and sending of sms
+  This endpoint expects the ff params:
+  - subscriber_number
+  - amount
+  """
 
-  msisdn = request.args['msisdn']
+  subscribe_number = request.args['subscriber_number']
   amount = request.args['amount']
 
   #: get the access token from DB
-  access_token = r.hget(msisdn, 'access_token')
-  print access_token
-  # HARDCODED STUFF huhu :((
-  #msisdn = '9175246984'
-  sender = '0680' # this is weird! 21580680
-  #access_token = 'tTDETIWF7yJtfdT_HlhMD53Ixqq26rSCHmdpJTO4TPY'
-  # Generate a random code for this user session
-  security_code = str(uuid.uuid4())[:6]
+  access_token = r.hget(subscriber_number, 'access_token')
 
-  params   = { 'access_token': access_token }
-  req_body = {
+  # check access token
+  if not access_token:
+    return jsonify(
+      dialog_url=const.G_DIALOG.format(app.config['GLOBE_APP_ID']),
+    )
+
+  sender = app.config['GLOBE_SHORTCODE'][-4:] # this is weird!
+  # Generate a random code for this user session
+  security_code = str(simpleflake())[-6:]
+
+  params = {'access_token': access_token}
+  req = {
     'outboundSMSMessageRequest': {
-      'clientCorrelator': '123',
-      'senderAddress': 'tel:%s' % sender,
+      'clientCorrelator': str(simpleflake()),
+      'senderAddress': 'tel:{}'.format(sender),
       'outboundSMSTextMessage': {
         'message': 'You will be charged PHP ' + amount + '. This is your code to proceed: ' + security_code
       },
-      'address': ['tel:+63%s' % msisdn],
+      'address': ['tel:{}'.format(subscriber_number)],
     }
   }
 
   res = requests.post(
-    const.G_SMS_ENDPOINT % sender,
+    const.G_SMS_ENDPOINT.format(sender),
     headers = {
       'Content-Type': 'application/json',
     },
     params = params,
-    data = json.dumps(req_body)
+    data = json.dumps(req)
   )
 
   if res.ok:
     #: Maybe log a little?
     # r.zadd(msisdn, time.localtime(), amount)
-    return "alright!"
+    return jsonify(**res.json())
+  else:
+    abort(500)
 
-  print res.content
-  return "CHARGE"
 # ==== END /charge ==== #
 
-if __name__ == '__main__':
-    app.run(host="0.0.0.0", debug=True)
 
-#test katpadillaaa@gmail as author
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", debug=True)
+
--- const.py
+++ const.py
@@ -1,8 +1,10 @@
-G_ENDPOINT = 'http://devapi.globelabs.com.ph/'
+G_ENDPOINT = 'http://devapi.globelabs.com.ph'
 G_VERSION  = 'v1'
 SMS_MSG    = "test msg"
 
 # end fuckin points
-G_CHARGING_ENDPOINT = G_ENDPOINT + 'payment/' + G_VERSION + '/transactions/amount'
-G_AUTH_ENDPOINT     = 'http://developer.globelabs.com.ph/oauth/access_token?'
-G_SMS_ENDPOINT      = 'http://devapi.globelabs.com.ph/smsmessaging/v1/outbound/%s/requests'
+G_DIALOG            = 'http://developer.globelabs.com.ph/dialog/oauth?app_id={}'
+G_AUTH_ENDPOINT     = 'http://developer.globelabs.com.ph/oauth/access_token'
+
+G_CHARGING_ENDPOINT = '{}/payment/{}/transactions/amount'.format(G_ENDPOINT, G_VERSION)
+G_SMS_ENDPOINT = '{}/smsmessaging/{}/outbound/'.format(G_ENDPOINT, G_VERSION) + '{}/requests'
